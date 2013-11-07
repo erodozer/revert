aa99d5ef4f34a83b19e776a11bda2816ec9f57e5
Nicholas Hydock <nhydock@gmail.com>
2013-11-07 14:33:37 -0500

    Throw in a holder class for Bullet logic

---
+++ src/com/kgp/game/Bullet.java
@@ -0,0 +1,13 @@
+package com.kgp.game;
+
+import com.kgp.imaging.ImagesLoader;
+import com.kgp.imaging.Sprite;
+
+public class Bullet extends Sprite {
+
+	public Bullet(int x, int y, int w, int h, ImagesLoader imsLd, String name) {
+		super(x, y, w, h, imsLd, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
