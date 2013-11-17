c0adcada17372366964fb2113e32de0279ed4c05
Nicholas Hydock <nhydock@gmail.com>
2013-11-16 21:36:43 -0500

    Add Game class that holds global variables of importance to the engine

---
+++ src/com/kgp/core/Game.java
@@ -0,0 +1,17 @@
+package com.kgp.core;
+
+public class Game {
+
+	protected static long period;
+	protected static int periodInMSec;
+	
+	public static long getPeriod()
+	{
+		return period;
+	}
+	
+	public static int getPeriodInMSec()
+	{
+		return periodInMSec;
+	}
+}
