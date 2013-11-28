78d4bb393e01db95f77bfa34db661489e95d6641
Nicholas Hydock <nhydock@gmail.com>
2013-11-28 16:06:13 -0500

    Add a game state for showing a report after a level objective is finished

--- src/com/kgp/core/GameState.java
+++ src/com/kgp/core/GameState.java
@@ -7,11 +7,33 @@ package com.kgp.core;
  */
 public enum GameState {
 
+	/**
+	 * Entire game process is paused
+	 */
 	ProcessPaused,
+	/**
+	 * Game logic is paused
+	 */
 	Paused,
+	/**
+	 * Game is running
+	 */
 	Active,
+	/**
+	 * Player has lost
+	 */
 	GameOver,
+	/**
+	 * Show the help screen
+	 */
 	Help, 
-	Start;
+	/**
+	 * Show the title screen
+	 */
+	Start, 
+	/**
+	 * Show player statistics after a level is over
+	 */
+	Report;
 	
 }
