86d47d699c759f3317ea122849b1b9d188566b61
Nicholas Hydock <nhydock@gmail.com>
2013-11-28 19:06:08 -0500

    Work a little on enemies, they now appear on screen!

--- src/revert/Entities/Enemy.java
+++ src/revert/Entities/Enemy.java
@@ -26,6 +26,7 @@ public class Enemy extends Actor {
 	public Enemy(World w, Player player) {
 		super(w, "enemy", new ArrayList<Actor>());
 		
+		this.hp = 3;
 		this.actors.add(player);
 	}
 	
@@ -36,7 +37,7 @@ public class Enemy extends Actor {
 
 	@Override
 	protected String getNextImage() {
-		return null;
+		return "enemy";
 	}
 
 	/**
@@ -121,8 +122,9 @@ public class Enemy extends Actor {
 	 * @return true if bullet has hit the enemy
 	 */
 	public boolean hit(Bullet b) {
-		if (b.getPosn().distance(this.getPosn()) < this.getCollissionRange())
+		if (this.getMyRectangle().contains(b.getPosn()))
 		{
+			System.out.println("hit");
 			if (b.getType() == this.getType())
 			{
 				this.hp--;
